1d7b4459a991a06f3a2a487b4a437f5c9852680b
brunomai <brunomai14@gmail.com>
2020-10-16 22:55:07 -0300

    Receitando 1.0
    Adesão ao controle de versão.

---
+++ Receitando/Receitando/Models/Analise.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Receitando.Model
+{
+    public class Analise
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get;  set; }
+
+        public DateTime data = DateTime.Now;
+        public  DateTime Data 
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+            
+         }
+
+        public string TextoCapturado { get; set; }
+
+        public bool PerfilAgressivo { get; set; }
+
+        public Analise()
+        {
+
+        }
+        public Analise(string textoCapturado,bool analiseTexto)
+        {
+            this.TextoCapturado = textoCapturado;
+            this.PerfilAgressivo = analiseTexto;
+        }
+
+    }
+}
