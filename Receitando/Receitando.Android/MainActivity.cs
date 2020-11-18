3335b13e7d740d2b148c7f746ea2c8dc27f9494b
brunomai <brunomai14@gmail.com>
2020-11-17 23:14:46 -0300

    Atualização Ícone do App

--- Receitando/Receitando.Android/MainActivity.cs
+++ Receitando/Receitando.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace Receitando.Droid
 {
-    [Activity(Label = "Receitando", Icon = "@drawable/cake", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "Receitando", Icon = "@drawable/ic_launcher_circle", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, IMessageSender
     {
         IMicrophoneService micService;
